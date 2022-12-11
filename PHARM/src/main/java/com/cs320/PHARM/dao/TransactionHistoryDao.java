ed81b0a069169562824b28afe3b2c986905b203b
tugceozgirgin <93055813+tugceozgirgin@users.noreply.github.com>
2022-12-11 17:27:28 +0300

    transactions

--- PHARM/src/main/java/com/cs320/PHARM/dao/TransactionHistoryDao.java
+++ PHARM/src/main/java/com/cs320/PHARM/dao/TransactionHistoryDao.java
@@ -1,4 +1,38 @@
 package com.cs320.PHARM.dao;
 
+import com.cs320.PHARM.model.TransactionHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class TransactionHistoryDao {
+    @Autowired
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<TransactionHistory> TransactionRowMapper = (resultSet, i) -> new TransactionHistory()
+            .DrugListId(resultSet.getInt("DrugListID"))
+            .PharmacyId(resultSet.getInt("PharmacyId"))
+            .DrugId(resultSet.getInt("DrugID"))
+            .Amount_Sold(resultSet.getInt("amount_sold"));
+
+
+    public TransactionHistoryDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertTransactionHistory(int drugListId, int pharmacyId,int drugId,int amount_sold){
+        jdbcTemplate.update("insert into transactionhistory(pharmacyid, druglistid, drugid, amount_sold) values(?,?,?,?)"
+                ,new Object[]{pharmacyId,drugListId,drugId,amount_sold});
+    }
+    public void deleteTransactionHistoryByIds(int drugListId, int pharmacyId,int drugId){
+        jdbcTemplate.update("DELETE from transactionhistory where druglistid=? and pharmacyid=? and drugid=?"
+                ,new Object[]{pharmacyId,drugListId,drugId});
+    }
+    public void updateSoldAmount(int drugListId, int pharmacyId,int drugId, int sold_amount){
+        jdbcTemplate.update("Update transactionhistory set amount_sold=? where druglistid=? and pharmacyid=? and drugid=?"
+                ,new Object[]{sold_amount,drugListId,pharmacyId,drugId});
+    }
+
 }
--- PHARM/src/main/java/com/cs320/PHARM/model/TransactionHistory.java
+++ PHARM/src/main/java/com/cs320/PHARM/model/TransactionHistory.java
@@ -1,4 +1,60 @@
 package com.cs320.PHARM.model;
 
 public class TransactionHistory {
-}
+
+        private int pharmacyId;
+        private int drugListId;
+        private int drugID;
+        private int amount_sold;
+
+        public TransactionHistory PharmacyId(int pharmacyId){
+            this.pharmacyId=pharmacyId;
+            return this;
+        }
+        public TransactionHistory DrugListId(int drugListId){
+            this.drugListId=drugListId;
+            return this;
+        }
+        public TransactionHistory DrugId(int drugID){
+            this.drugID=drugID;
+            return this;
+        }
+        public TransactionHistory Amount_Sold(int amount_sold){
+            this.amount_sold=amount_sold;
+            return this;
+        }
+
+
+        public int getPharmacyId() {
+            return pharmacyId;
+        }
+
+        public void setPharmacyId(int pharmacyId) {
+            this.pharmacyId = pharmacyId;
+        }
+
+        public int getDrugListId() {
+            return drugListId;
+        }
+
+        public void setDrugListId(int drugListId) {
+            this.drugListId = drugListId;
+        }
+
+        public int getDrugID() {
+            return drugID;
+        }
+
+        public void setDrugID(int drugID) {
+            this.drugID = drugID;
+        }
+
+        public int getAmount_sold() {
+            return amount_sold;
+        }
+
+        public void setAmount_sold(int amount_sold) {
+            this.amount_sold = amount_sold;
+        }
+    }
+
